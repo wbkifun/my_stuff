5defbac094b3adbff0b3216d41b604d7cf8513f1
Ki-Hwan Kim <kh.kim@kiaps.org>
2012-11-19 10:10:57 +0900

    rm the deleted fields

--- code_examples/matplotlib_rcparams.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python
-
-import matplotlib as mpl
-
-for key, val in mpl.rcParams.items():
-    print key, val
--- code_examples/wave2d/core_numpy.py
+++
@@ -1,7 +0,0 @@
-from __future__ import division
-
-
-si = slice(1,-1)
-def advance(c, f, g):
-	f[si,si] = c[si,si]*(g[2:,si] + g[:-2,si] + g[si,2:] + g[si,:-2] - 4*g[si,si]) \
-            + 2*g[si,si] - f[si,si]
