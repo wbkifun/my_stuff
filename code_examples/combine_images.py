c84c99d7d96f8a14af938e2eb3d02a3fcaa4a7e9
Ki-Hwan Kim <kh.kim@kiaps.org>
2013-04-11 14:08:22 +0900

    modify the combine_images.py

--- code_examples/combine_images.py
+++ code_examples/combine_images.py
@@ -1,8 +1,9 @@
+from __future__ import division
 import Image, ImageDraw, ImageFont
 import sys
 
 
-N1, N2 = 27, 30
+N = 30
 tmax = 1000
 tgap = 1
 
@@ -11,16 +12,18 @@ for tstep in xrange(tgap, tmax+1, tgap):
     print 'tstep=\t%d/%d (%1.2f %s)\r' % (tstep, tmax, tstep/tmax*100, '%'),
     sys.stdout.flush()
 
-    im1 = Image.open('../png_N%d_pole/%.6d.png' % (N1, tstep))
-    im2 = Image.open('../png_N%d_pole/%.6d.png' % (N2, tstep))
+    im1 = Image.open('png_pole/%.6d.png' % (tstep))
+    im2 = Image.open('png_pole_modified/%.6d.png' % (tstep))
 
     cim = Image.new('RGB', (1600,800))
     cim.paste(im1, (0,0))
     cim.paste(im2, (800,0))
 
+    '''
     font = ImageFont.truetype('/usr/share/fonts/truetype/ttf-dejavu/DejaVuSans.ttf', 30)
     draw = ImageDraw.Draw(cim)
     draw.text((50,10), 'N=%d' % N1, font=font)
     draw.text((850,10), 'N=%d' % N2, font=font)
+    '''
 
-    cim.save('./%.6d.png' % (tstep))
+    cim.save('png_pole_compare/%.6d.png' % (tstep))
