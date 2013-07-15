7659b663d892d92339fdc8ac4647e6fc54ec81e7
Ki-Hwan Kim <kh.kim@kiaps.org>
2013-07-16 03:37:54 +0900

    gather the file into code_examples/Input_Output/

---
+++ code_examples/Input_Ouput/netcdf4_test_fortran_order.py
@@ -0,0 +1,87 @@
+from __future__ import division
+import numpy
+from numpy.testing import assert_array_equal as assert_ae
+import netCDF4 as nc
+
+
+
+#------------------------------------------------------------------------------
+# variables
+#------------------------------------------------------------------------------
+nx, ny = 3, 4
+
+data = numpy.arange(nx*ny, dtype='f8')
+data_c = data.reshape((nx,ny), order='C')
+data_f = data.reshape((nx,ny), order='F')
+
+print "data_c.ravel('A')", data_c.ravel('A')
+print "data_c.ravel('C')", data_c.ravel('C')
+print "data_c.ravel('F')", data_c.ravel('F')
+print '-'*80
+print "data_f.ravel('A')", data_f.ravel('A')
+print "data_f.ravel('C')", data_f.ravel('C')
+print "data_f.ravel('F')", data_f.ravel('F')
+
+
+#------------------------------------------------------------------------------
+# write a nc file
+#------------------------------------------------------------------------------
+grp = nc.Dataset('write_read.nc', 'w', format='NETCDF4')
+
+# attributes
+grp.description = 'netCDF4 write/read test'
+
+# dimensions
+nx_d = grp.createDimension('nx', nx)
+ny_d = grp.createDimension('ny', ny)
+
+# variables
+vc_v = grp.createVariable('vc', 'f8', ('nx','ny'))
+vf_v = grp.createVariable('vf', 'f8', ('nx','ny'))
+
+# write data to variables
+vc_v[:] = data_c
+vf_v[:] = data_f
+
+grp.close()
+
+
+
+#------------------------------------------------------------------------------
+# read a nc file
+#------------------------------------------------------------------------------
+f = nc.Dataset('write_read.nc', 'r', format='NETCDF4')
+
+print '='*80
+print f.description
+
+dim_nx = len( f.dimensions['nx'] )
+dim_ny = len( f.dimensions['ny'] )
+
+read_c = f.variables['vc'][:]
+read_f = f.variables['vf'][:]
+read_f2 = f.variables['vf'][:].reshape((dim_nx, dim_ny), order='F')
+read_f3 = numpy.zeros((dim_nx, dim_ny), order='F')
+read_f3[:] = f.variables['vf'][:]
+
+print "read_c.ravel('A')", read_c.ravel('A')
+print "read_c.ravel('C')", read_c.ravel('C')
+print "read_c.ravel('F')", read_c.ravel('F')
+print '-'*80
+print "read_f.ravel('A')", read_f.ravel('A')
+print "read_f.ravel('C')", read_f.ravel('C')
+print "read_f.ravel('F')", read_f.ravel('F')
+print '-'*80
+print "read_f2.ravel('A')", read_f2.ravel('A')
+print "read_f2.ravel('C')", read_f2.ravel('C')
+print "read_f2.ravel('F')", read_f2.ravel('F')
+print '-'*80
+print "read_f3.ravel('A')", read_f3.ravel('A')
+print "read_f3.ravel('C')", read_f3.ravel('C')
+print "read_f3.ravel('F')", read_f3.ravel('F')
+print '-'*80
+
+assert_ae(data_c.ravel('A'), read_c.ravel('A'))
+assert_ae(data_f.ravel('A'), read_f3.ravel('A'))
+
+f.close()
---
+++ code_examples/Input_Ouput/netcdf4_write_read.py
@@ -0,0 +1,71 @@
+from __future__ import division
+import numpy
+import netCDF4 as nc
+from pprint import pprint
+from numpy.testing import assert_array_equal as assert_ae
+
+
+
+#------------------------------------------------------------------------------
+# variables
+#------------------------------------------------------------------------------
+nx, ny = 3, 4
+
+arr_a = numpy.random.rand(nx,ny)
+arr_b = numpy.random.rand(nx,ny)
+
+print 'write arrays'
+pprint(arr_a)
+pprint(arr_b)
+
+
+#------------------------------------------------------------------------------
+# write a nc file
+#------------------------------------------------------------------------------
+ncf = nc.Dataset('write_read.nc', 'w', format='NETCDF4')
+
+# attributes
+ncf.description = 'netCDF4 write/read test'     # string
+ncf.size = nx*ny                                # number
+
+# dimensions
+ncf.createDimension('nx', nx)
+ncf.createDimension('ny', ny)
+
+# variables
+va = ncf.createVariable('va', 'f8', ('nx','ny'))
+vb = ncf.createVariable('vb', 'f8', ('nx','ny'))
+
+# write data to variables
+va[:] = arr_a
+vb[:] = arr_b
+
+ncf.close()
+
+
+
+#------------------------------------------------------------------------------
+# read a nc file
+#------------------------------------------------------------------------------
+ncf = nc.Dataset('write_read.nc', 'r', format='NETCDF4')
+
+print '='*80
+print 'description', ncf.description
+print 'size', ncf.size
+
+dim_nx = len( ncf.dimensions['nx'] )
+dim_ny = len( ncf.dimensions['ny'] )
+
+read_a = ncf.variables['va'][:]
+read_b = ncf.variables['vb'][:]
+
+print 'read arrays'
+pprint(read_a)
+pprint(read_b)
+
+
+# verification
+assert_ae(read_a, arr_a)
+assert_ae(read_b, arr_b)
+
+ncf.close()
