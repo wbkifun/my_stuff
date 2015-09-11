dc0552f6e80c5ccdef9b9076bbc78783e83bfc5b
Ki-Hwan Kim <kh.kim@kiaps.org>
2015-09-11 14:05:18 +0900

    Change to class CubedSphereGrid for unittest

--- code_examples/Cubed_sphere/test_cube_grid.py
+++ code_examples/Cubed_sphere/test_cube_grid.py
@@ -1,8 +1,9 @@
 #------------------------------------------------------------------------------
-# filename  : check_cs_grid.py
+# filename  : test_cube_grid.py
 # author    : Ki-Hwan Kim  (kh.kim@kiaps.org)
 # affilation: KIAPS (Korea Institute of Atmospheric Prediction Systems)
 # update    : 2015.9.8      srart
+#             2015.9.11     change to unittest
 #
 #
 # description: 
@@ -13,28 +14,22 @@ from __future__ import division
 import numpy as np
 import netCDF4 as nc
 import sys
-import re
 
 from numpy.testing import assert_equal as equal
 from numpy.testing import assert_array_equal as a_equal
 from numpy.testing import assert_array_almost_equal as aa_equal
 
+from cube_grid import CubedSphereGrid
 
 
 
-def check_consistency_mvps(ncf):
-    print 'check consistency: mvps...'
 
-    size = len( ncf.dimensions['size'] )
-    mvps = ncf.variables['mvps'][:]
-
-    exclude_neg = lambda arr, seq: [k for k in arr[seq] if k != -1]
-
-    for seq in xrange(size):
-        mvp0 = exclude_neg(mvps, seq)
+def check_consistency_mvps(mvps):
+    for seq, mvp in enumerate(mvps):
+        mvp0 = [k for k in mvp if k != -1]
 
         for m in mvp0:
-            mvp1 = exclude_neg(mvps, m)
+            mvp1 = [k for k in mvps[m] if k != -1]
 
             for i in xrange(4):
                 mvp1_roll = np.roll(mvp1,i)
@@ -44,38 +39,20 @@ def check_consistency_mvps(ncf):
 
 
 
-
-def check_consistency_uids_gids(ncf):
-    print 'check consistency: is_uvps, uids and gids...'
-
-    size = len( ncf.dimensions['size'] )
-    is_uvps = ncf.variables['is_uvps'][:]
-    uids = ncf.variables['uids'][:]
-    gids = ncf.variables['gids'][:]
-
+def check_consistency_uids_gids(is_uvps, uids, gids):
     u_seq = 0
-    for seq in xrange(size):
-        if is_uvps[seq]:
+    for seq, is_uvp in enumerate(is_uvps):
+        if is_uvp:
             equal(uids[seq], u_seq)
             equal(gids[u_seq], seq)
             u_seq += 1
 
 
 
-
-def check_indices_nbrs(ncf):
-    print 'check indices: nbrs...'
-
-    ne = len( ncf.dimensions['ne'] )
-    ngq = len( ncf.dimensions['ngq'] )
-    size = len( ncf.dimensions['size'] )
-    gq_indices = ncf.variables['gq_indices'][:]
-    nbrs = ncf.variables['nbrs'][:]
-
+def check_indices_nbrs(ne, ngq, gq_indices, nbrs):
     ij2seq = dict()
-    for seq in xrange(size):
-        ij = tuple(gq_indices[seq])
-        ij2seq[ij] = seq
+    for seq, ij in enumerate(gq_indices):
+        ij2seq[tuple(ij)] = seq
 
     a_equal(nbrs[ ij2seq[(1,1,1,1,1)] ], 
             [ij2seq[(1,1,1,2,1)], ij2seq[(1,1,1,2,2)], \
@@ -128,14 +105,64 @@ def check_indices_nbrs(ncf):
 
 
 
+def test_consistency_mvps_3_4():
+    '''
+    CubedSphereGrid: check consistency of mvps (ne=3,ngq=4)
+    '''
+    ne, ngq = 3, 4
+    csgrid = CubedSphereGrid(ne, ngq)
+    check_consistency_mvps(csgrid.mvps)
+
+
+
+def test_consistency_uids_gids_3_4():
+    '''
+    CubedSphereGrid: check consistency of is_uvps, uids and gids (ne=3,ngq=4)
+    '''
+    ne, ngq = 3, 4
+    csgrid = CubedSphereGrid(ne, ngq)
+    check_consistency_uids_gids(csgrid.is_uvps, csgrid.uids, csgrid.gids)
+
+
+
+def test_indices_nbrs_3_4():
+    '''
+    CubedSphereGrid: check indices of nbrs (ne=3,ngq=4)
+    '''
+    ne, ngq = 3, 4
+    csgrid = CubedSphereGrid(ne, ngq)
+    check_indices_nbrs(ne, ngq, csgrid.gq_indices, csgrid.nbrs)
+
+
+
+
 if __name__ == '__main__':
-    fpath = sys.argv[1]
+    import argparse
+    import re
+
+    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('cs_grid_file', type=str, help='cubed-sphere grid NetCDF file')
+    args = parser.parse_args()
+    fpath = args.cs_grid_file
 
     ne = int( re.search('ne([0-9]+)',fpath).group(1) )
     ngq = int( re.search('ngq([0-9]+)',fpath).group(1) )
     print 'ne=%d, ngq=%d'%(ne, ngq) 
 
+
     ncf = nc.Dataset(fpath, 'r', format='NETCDF4')
-    check_consistency_mvps(ncf)
-    check_consistency_uids_gids(ncf)
-    check_indices_nbrs(ncf)
+    mvps = ncf.variables['mvps'][:]
+    is_uvps = ncf.variables['is_uvps'][:]
+    uids = ncf.variables['uids'][:]
+    gids = ncf.variables['gids'][:]
+    gq_indices = ncf.variables['gq_indices'][:]
+    nbrs = ncf.variables['nbrs'][:]
+
+    print 'check consistency: mvps...'
+    check_consistency_mvps(mvps)
+
+    print 'check consistency: is_uvps, uids and gids...'
+    check_consistency_uids_gids(is_uvps, uids, gids)
+
+    print 'check indices: nbrs...'
+    check_indices_nbrs(ne, ngq, gq_indices, nbrs)
