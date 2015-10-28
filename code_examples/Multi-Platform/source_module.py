e260ecf9a2c0c5857cf2e3da8ac1776c629c84ea
Ki-Hwan Kim <kh.kim@kiaps.org>
2015-10-28 09:17:05 +0900

    Add make_signature_f90() and make_signature_c in source_moduly.py

--- code_examples/Multi-Platform/source_module.py
+++ code_examples/Multi-Platform/source_module.py
@@ -4,6 +4,7 @@
 # affilation: System Configuration Team, KIAPS
 # update    : 2015.3.18    start
 #             2015.9.23    modify the case of CPU-C with f2py
+#             2015.10.27   add make_signature_f90() and make_signature_c()
 #
 #
 # description:
@@ -16,12 +17,23 @@ import shutil
 import subprocess as subp
 import sys
 import tempfile
+import parse
+import numpy as np
 
 
 
 fcompiler = 'gnu95'     # f2py Fortran, $ f2py -c --help-fcompiler
 compiler = 'unix'       # f2py C, $ f2py -c --help-compiler
 
+
+
+sig_template = '''
+python module $MODNAME
+  interface
+CONTENT
+  end interface
+end python module
+'''
 
 
 
@@ -90,3 +102,72 @@ def get_module_f90(src, pyf):
 
 def get_module_c(src, pyf):
     return get_module(src, pyf, 'c')
+
+
+
+
+def make_signature_f90(src):
+    lines = list()
+    for line in src.split('\n'):
+        ll = line.lower()
+        if 'subroutine' in ll or 'intent(' in ll:
+            ll = ll.replace('integer, ', 'integer, required, ')
+            lines.append(' '*4 + ll)
+        
+    sig = sig_template.replace('CONTENT', '\n'.join(lines))
+
+    return sig
+
+
+
+
+def make_signature_c(src):
+    pc = parse.compile('void {}({}) {\n')
+
+    contents = list()
+    lines = list()
+    for line in src.split('\n'):
+        ll = line.lower()
+
+        result = parse.parse('void {}({}) {', ll)
+        if result != None:
+            funcname, args = result.fixed
+            arg_list = [arg.strip().split(' ') for arg in args.split(',')]
+            arg_names = [arg[1].strip('*') for arg in arg_list]
+            arg_types = [{'int':'integer', 'double':'real(8)'}[arg[0]] \
+                    for arg in arg_list]
+            is_array = [arg[0].endswith('*') or arg[1].startswith('*') \
+                    for arg in arg_list]
+
+            lines.append('    subroutine %s(%s)'%(funcname, ', '.join(arg_names)))
+            lines.append('      intent(c) :: %s'%(funcname))
+            lines.append('      intent(c)')
+
+            for seq, (arg_name, arg_type) in enumerate(zip(arg_names,arg_types)):
+                if is_array[seq]:
+                    lines.append('      %s, dimension(SIZE%s), intent(IO%s) :: %s'%(arg_type, arg_name, arg_name, arg_name))
+                elif arg_type == 'integer':
+                    lines.append('      integer, required, intent(in) :: %s'%(arg_name))
+                else:
+                    lines.append('      %s, intent(in) :: %s'%(arg_type, arg_name))
+
+            lines.append('    end subroutine')
+            content = '\n'.join(lines)
+            count_replace = 0
+
+        if '::' in line:
+            result2 = parse.parse('    // {} :: {}, {}', ll)
+            if result2 != None:
+                name, size, io = result2.fixed
+                assert io in ['in', 'inout']
+                content = content.replace('SIZE%s'%(name), size)
+                content = content.replace('IO%s'%(name), io)
+
+                count_replace += 1
+                if count_replace == np.count_nonzero(is_array):
+                    contents.append(content)
+                    lines = list()
+
+    sig = sig_template.replace('CONTENT', '\n'.join(contents))
+
+    return sig
--- code_examples/Multi-Platform/test_source_module.py
+++ code_examples/Multi-Platform/test_source_module.py
@@ -9,11 +9,10 @@ from nose.tools import raises, ok_
 
 
 
-from source_module import get_module_f90, get_module_c
-
-
 
 def test_get_module_f90():
+    from source_module import get_module_f90
+
     # setup
     nx = 1000000
     a = np.random.rand(nx)
@@ -41,8 +40,8 @@ python module $MODNAME
   interface
     subroutine add(n,a,b,c)
       integer, required, intent(in) :: n
-      double precision, intent(in) :: a(n), b(n)
-      double precision, intent(inout) :: c(n)
+      real(8), intent(in) :: a(n), b(n)
+      real(8), intent(inout) :: c(n)
     end subroutine
   end interface
 end python module
@@ -58,6 +57,8 @@ end python module
 
 
 def test_get_module_c():
+    from source_module import get_module_c
+
     # setup
     nx = 1000000
     a = np.random.rand(nx)
@@ -93,3 +94,117 @@ end python module
 
     add_c(nx, a, b, c)
     a_equal(a+b, c)
+
+
+
+
+def test_make_signature_f90():
+    from source_module import make_signature_f90
+
+    src_f = '''
+SUBROUTINE add(nx, a, b, c)
+  IMPLICIT NONE
+  INTEGER, INTENT(IN) :: nx
+  REAL(8), DIMENSION(nx), INTENT(IN) :: a, b
+  REAL(8), DIMENSION(nx), INTENT(INOUT) :: c
+
+  INTEGER :: ii
+
+  DO ii=1,nx
+    c(ii) = a(ii) + b(ii)
+  END DO
+END SUBROUTINE
+
+
+SUBROUTINE sub(nx, a, b, c)
+  IMPLICIT NONE
+  INTEGER, INTENT(IN) :: nx
+  REAL(8), DIMENSION(nx), INTENT(IN) :: a, b
+  REAL(8), DIMENSION(nx), INTENT(INOUT) :: c
+
+  INTEGER :: ii
+
+  DO ii=1,nx
+    c(ii) = a(ii) - b(ii)
+  END DO
+END SUBROUTINE
+    '''
+
+    ref_sig_f = '''
+python module $MODNAME
+  interface
+    subroutine add(nx, a, b, c)
+      integer, required, intent(in) :: nx
+      real(8), dimension(nx), intent(in) :: a, b
+      real(8), dimension(nx), intent(inout) :: c
+    end subroutine
+    subroutine sub(nx, a, b, c)
+      integer, required, intent(in) :: nx
+      real(8), dimension(nx), intent(in) :: a, b
+      real(8), dimension(nx), intent(inout) :: c
+    end subroutine
+  end interface
+end python module
+'''
+
+    sig_f = make_signature_f90(src_f)
+    equal(ref_sig_f, sig_f)
+
+
+
+
+def test_make_signature_c():
+    from source_module import make_signature_c
+
+    src_c = '''
+void add(int nx, double *a, double *b, double *c) {
+    // size and intent of array arguments for f2py
+    // a :: nx, in
+    // b :: nx, in
+    // c :: nx, inout
+
+    int i;
+
+    for (i=0; i<nx; i++) {
+        c[i] = a[i] + b[i];
+    }
+}
+
+void dummy(int nx, double k, double *a, double *b) {
+    // size and intent of array arguments for f2py
+    // a :: nx, in
+    // b :: nx, inout
+
+    int i;
+
+    for (i=0; i<nx; i++) {
+        b[i] = a[i] + k*b[i];
+    }
+}
+    '''
+
+    ref_sig_c = '''
+python module $MODNAME
+  interface
+    subroutine add(nx, a, b, c)
+      intent(c) :: add
+      intent(c)
+      integer, required, intent(in) :: nx
+      real(8), dimension(nx), intent(in) :: a
+      real(8), dimension(nx), intent(in) :: b
+      real(8), dimension(nx), intent(inout) :: c
+    end subroutine
+    subroutine dummy(nx, k, a, b)
+      intent(c) :: dummy
+      intent(c)
+      integer, required, intent(in) :: nx
+      real(8), intent(in) :: k
+      real(8), dimension(nx), intent(in) :: a
+      real(8), dimension(nx), intent(inout) :: b
+    end subroutine
+  end interface
+end python module
+'''
+
+    sig_c = make_signature_c(src_c)
+    equal(ref_sig_c, sig_c)
