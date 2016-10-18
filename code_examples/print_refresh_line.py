b3adfd1cf38d43e741c7552895379ca26aa73c49
Ki-Hwan Kim <kh.kim@kiaps.org>
2016-10-18 16:31:00 +0900

    Add print_refresh_line.py at code_examples

---
+++ code_examples/print_refresh_line.py
@@ -0,0 +1,36 @@
+from time import sleep, localtime, strftime, gmtime
+from datetime import datetime, timedelta
+
+
+
+
+def print_progress(t0, step, max_step):
+    '''
+    Assume step starts from 0
+    '''
+    step += 1
+    dt = datetime.now() - t0
+    step_width = max(len(str(max_step)), 4)
+
+    if step == 1:
+        print('[{:>{ms}}] [progress] [elapsed time] [estimated end time]'.format(
+            'step', ms=step_width))
+
+    print('[{:>4d}] [{:>6.1f} %] [{:>12s}] [{:>18s}]'.format(\
+            step, 
+            (step)/max_step*100, 
+            str(timedelta( seconds=int(dt.seconds) )), 
+            str(timedelta( seconds=int(max_step*dt.seconds/step) )) ), # estimated end time
+            flush=True, end='\r')
+
+
+
+if __name__ == '__main__':
+    tmax = 120
+    t0 = datetime.now()
+
+    for tstep in range(tmax):
+        print_progress(t0, tstep, tmax)
+        sleep(0.7)
+
+    print()
